39090dc6db35e8fd3685fb23e32ae32e6298ecf8
Paulmicha <paul.michalet@gmail.com>
2019-06-04 17:54:58 -0300

    [wip] Experimental : use the same app folder for backend, frontend, and bff (backend for frontend).

--- scripts/cwt/extend/cwt/bootstrap.docker-compose.hook.sh
+++ scripts/cwt/extend/cwt/bootstrap.docker-compose.hook.sh
@@ -10,5 +10,8 @@
 # @see cwt/extensions/docker-compose/cwt/pre_bootstrap.docker-compose.hook.sh
 #
 
-# alias php="docker-compose exec $DC_TTY php php"
-# alias composer="docker-compose exec $DC_TTY php composer"
+alias node="docker-compose exec $DC_TTY nodejs node"
+alias npm="docker-compose exec $DC_TTY nodejs npm"
+alias npx="docker-compose exec $DC_TTY nodejs npx"
+alias php="docker-compose exec $DC_TTY php php"
+alias composer="docker-compose exec $DC_TTY php composer"
--- scripts/cwt/extend/stack/global.docker-compose.vars.sh
+++ scripts/cwt/extend/stack/global.docker-compose.vars.sh
@@ -14,3 +14,29 @@
 # Generate and write the docker-compose.yml file in project docroot.
 global DC_MODE 'generate'
 global DC_YML_VARIANTS "$INSTANCE_TYPE $HOST_TYPE"
+
+# The node container's "start" command depends on the local instance type.
+# Convention : name the command after INSTANCE_TYPE value (i.e. dev, stage, or
+# prod).
+global NODE_COMMAND "sh -c 'npm install && npm run $INSTANCE_TYPE'"
+
+# Use latest images as of 2019/06/04.
+# See https://github.com/wodby/docker4php/blob/master/.env
+global POSTGRES_TAG '11-1.6.3'
+global NODE_TAG='12-0.16.0'
+# global PHP_TAG '5.6-dev-4.11.12'
+global PHP_TAG '7.3-dev-4.11.12'
+global NGINX_TAG '1.15-5.3.0'
+global ADMINER_TAG='4-3.5.12'
+# global ATHENAPDF_TAG='2.10.0'
+# global RSYSLOG_TAG='latest'
+# global WEBGRIND_TAG='1.5-1.9.12'
+
+# Dump DB credentials for local dev.
+# TODO [prod-check] Proper DB credentials management.
+global DB_HOST='postgres'
+global DB_DRIVER='pgsql'
+global DB_NAME='app'
+global DB_USER='app'
+global DB_PASSWORD='app'
+global DB_ROOT_PASSWORD='app'
